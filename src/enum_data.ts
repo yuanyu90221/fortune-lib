dfd679682f95e58ced795ebf59cc2871833b4f95
yuanyu <yuanyu90221@gmail.com>
2018-10-29 18:10:30 +0800

    add getOrder

---
+++ src/enum_data.ts
@@ -0,0 +1,6 @@
+enum EARTH_SYMBOL{
+  "子", "寅", "辰", "午", "申", "戌",
+  "未", "巳", "卯", "丑", "亥","酉"
+};
+
+export {EARTH_SYMBOL};
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import {EARTH_SYMBOL} from './enum_data';
+const earth_symbol_order = ["子", "寅", "辰", "午", "申", "戌"];
+const earth_symbol_reverse = ["未", "巳", "卯", "丑", "亥", "酉"];
+
+const getNextOrder = (symbol: string) => {
+  let idx = earth_symbol_order.indexOf(symbol);
+  if (idx == -1) {
+    throw `${symbol} not in earth symbol order`
+  }
+  let currentIdx = (idx+1) % earth_symbol_order.length;
+  return earth_symbol_order[currentIdx];
+};
+
+const getNextReverseOrder = (symbol: string) => {
+  let idx = earth_symbol_reverse.indexOf(symbol);
+  if (idx == -1) {
+    throw `${symbol} not in earth symbol reverse order`
+  }
+  let currentIdx = (idx+1) % earth_symbol_reverse.length;
+  return earth_symbol_reverse[currentIdx];
+};
+
+export {getNextOrder, getNextReverseOrder};
