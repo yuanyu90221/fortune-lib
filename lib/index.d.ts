24e713d1aa498e411078515b2f1d458e2316e2f9
yuanyu <yuanyu90221@gmail.com>
2018-11-04 22:29:47 +0800

    update

---
+++ lib/index.d.ts
@@ -0,0 +1,14 @@
+import { EARTH_SYMBOL } from './enum_data';
+/**
+ * @description getNextOrder
+ *
+ * @param {string} symbol
+ */
+declare const getNextOrder: (symbol: string) => string;
+/**
+ * @description getNextReverseOrder
+ *
+ * @param {string} symbol
+ */
+declare const getNextReverseOrder: (symbol: string) => string;
+export { getNextOrder, getNextReverseOrder, EARTH_SYMBOL };
