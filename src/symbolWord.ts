99eb7ca8801ec5a1c8600c8850e7f9beba475c67
yuanyu <yuanyu90221@gmail.com>
2018-11-06 21:24:03 +0800

    add symbolWord.ts

---
+++ src/symbolWord.ts
@@ -0,0 +1,27 @@
+export default class symbolWord {
+
+  private symbol: String = '';
+  private reverse: any;
+  private order: any;
+  constructor({symbol='', reverse = {}, order = {}}) {
+    this.symbol = symbol;
+    this.reverse = reverse;
+    this.order = order;
+  }
+  public next() {
+    return this.order;
+  }
+  public getSymbol() {
+    return this.symbol;
+  }
+  public reverseOrder() {
+    return this.reverse;
+  }
+
+  public setOrder(order = {}) {
+    this.order = order;
+  }
+  public setReverse(reverse = {}){
+    this.reverse = reverse;
+  }
+}
