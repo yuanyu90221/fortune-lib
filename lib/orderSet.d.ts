0e58eedeede19083edd8c297bf21cce2150248e6
yuanyu <yuanyu90221@gmail.com>
2018-11-27 17:21:06 +0800

    add OrderSet test

--- lib/orderSet.d.ts
+++ lib/orderSet.d.ts
@@ -1,4 +1,16 @@
-import { EARTH_SYMBOL_ORDER, EARTH_SYMBOL_REVERSE } from './enum_data';
+import { EARTH_SYMBOL_ORDER, EARTH_SYMBOL_REVERSE, EARTH_SYMBOL } from './enum_data';
+/**
+ * @description transferNextOrder
+ *
+ * @param {EARTH_SYMBOL} symbol
+ */
+declare const transferNextOrder: (symbol: EARTH_SYMBOL) => EARTH_SYMBOL;
+/**
+ * @description transferReverseOrder
+ *
+ * @param {EARTH_SYMBOL} symbol
+ */
+declare const transferReverseOrder: (symbol: EARTH_SYMBOL) => EARTH_SYMBOL;
 /**
  * @description getNextOrder
  *
@@ -11,4 +23,4 @@ declare const getNextOrder: (symbol: EARTH_SYMBOL_ORDER) => EARTH_SYMBOL_ORDER;
  * @param {EARTH_SYMBOL_REVERSE} symbol
  */
 declare const getNextReverseOrder: (symbol: EARTH_SYMBOL_REVERSE) => EARTH_SYMBOL_REVERSE;
-export { getNextOrder, getNextReverseOrder, EARTH_SYMBOL_ORDER, EARTH_SYMBOL_REVERSE };
+export { getNextOrder, getNextReverseOrder, transferNextOrder, transferReverseOrder, EARTH_SYMBOL_ORDER, EARTH_SYMBOL_REVERSE };
--- test/OrderSetTest.ts
+++ test/OrderSetTest.ts
@@ -1,4 +1,4 @@
-import {OrderSetUtil, EARTH_SYMBOL_ORDER, EARTH_SYMBOL_REVERSE} from '../lib/index';
+import {OrderSetUtil, EARTH_SYMBOL_ORDER, EARTH_SYMBOL_REVERSE, EARTH_SYMBOL} from '../lib/index';
 
 test('OrderSetTest', () => {
   let result: EARTH_SYMBOL_REVERSE = OrderSetUtil.getNextReverseOrder(EARTH_SYMBOL_REVERSE.丑);
@@ -7,4 +7,13 @@ test('OrderSetTest', () => {
   let resultNext: EARTH_SYMBOL_ORDER = OrderSetUtil.getNextOrder(EARTH_SYMBOL_ORDER.午);
   expect(EARTH_SYMBOL_ORDER[resultNext]).toEqual(EARTH_SYMBOL_ORDER[EARTH_SYMBOL_ORDER.申]);
   console.log(EARTH_SYMBOL_ORDER[resultNext]);
+  
+  let resultTestNext: EARTH_SYMBOL = OrderSetUtil.transferReverseOrder(EARTH_SYMBOL.丑);
+  expect(EARTH_SYMBOL[resultTestNext]).toEqual(EARTH_SYMBOL[EARTH_SYMBOL.卯]);
+  console.log(EARTH_SYMBOL[resultTestNext]);
+
+  let resultTest: EARTH_SYMBOL = OrderSetUtil.transferNextOrder(EARTH_SYMBOL.午);
+  expect(EARTH_SYMBOL[resultTest]).toEqual(EARTH_SYMBOL[EARTH_SYMBOL.申]);
+  console.log(EARTH_SYMBOL[resultTest]);
+  
 });
