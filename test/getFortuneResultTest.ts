bf4e7508fc199303f223787c34db625bf619ba76
JsonLiang <json@cbx.io>
2019-10-07 17:35:59 +0800

    update

--- test/getFortuneResultTest.ts
+++ test/getFortuneResultTest.ts
@@ -1,11 +1,11 @@
 import {CoinSet, CoinSymbol, FaceSymbol, FortuneCS, FIVE_ELEMENT, getFortuneResult} from '../lib/index';
 import moment from 'moment';
 test('testFortuneLib', ()=>{
-  let coSet6 = new CoinSet([FaceSymbol.FaceUp, FaceSymbol.FaceUp, FaceSymbol.FaceUp]);
-  let coSet5 = new CoinSet([FaceSymbol.FaceDown, FaceSymbol.FaceUp, FaceSymbol.FaceUp]);
+  let coSet6 = new CoinSet([0, FaceSymbol.FaceUp, FaceSymbol.FaceUp]);
+  let coSet5 = new CoinSet([0,0,1]);
   let coSet4 = new CoinSet([FaceSymbol.FaceUp, FaceSymbol.FaceDown, FaceSymbol.FaceUp]);
   let upbsSet = [coSet6, coSet5, coSet4];
-  let coSet3 = new CoinSet([FaceSymbol.FaceUp, FaceSymbol.FaceDown, FaceSymbol.FaceDown]);
+  let coSet3 = new CoinSet([1, 1, 1]);
   let coSet2 = new CoinSet([FaceSymbol.FaceUp, FaceSymbol.FaceDown, FaceSymbol.FaceUp]);
   let coSet1 = new CoinSet([FaceSymbol.FaceUp, FaceSymbol.FaceUp, FaceSymbol.FaceDown]);
   let bsSet = [coSet3, coSet2, coSet1];
